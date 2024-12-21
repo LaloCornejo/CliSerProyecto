84ef73e2e32c915cdc314e95645a27982f60b567
Lalo Cornejo <lalocornejoofficial@gmail.com>
2024-12-21 18:06:23 +0100

    v0.3.2

--- client.cpp
+++ client.cpp
@@ -14,122 +14,145 @@ void clearScreen() {
 }
 
 void frontPage() {
-  clearScreen();
-  printf("\n==- Trivia Quiz Game -==\n");
-  printf("+++++++++++++++++++++++++++++\n");
-  printf("Menu\n");
-  printf("1. Comincia una sessione di trivia\n");
-  printf("2. Esci\n");
-  printf("La tua scelta: ");
+    clearScreen();
+    printf("\n==- Trivia Quiz Game -==\n");
+    printf("+++++++++++++++++++++++++++++\n");
+    printf("Menu\n");
+    printf("1. Comincia una sessione di trivia\n");
+    printf("2. Esci\n");
+    printf("La tua scelta: ");
 }
 
 void sNickname() {
-  clearScreen();
-  printf("Trivia Quiz\n");
-  printf("+++++++++++++++++++++++++++++\n");
-  printf("Scegli un nickname (deve essere univoco): ");
+    clearScreen();
+    printf("Trivia Quiz\n");
+    printf("+++++++++++++++++++++++++++++\n");
+    printf("Scegli un nickname (deve essere univoco): ");
 }
 
 void sTheme() {
-  clearScreen();
-  printf("Quiz disponibili\n");
-  printf("+++++++++++++++++++++++++++++\n");
-  printf("1 - Curiosita sulla tecnologia\n");
-  printf("2 - Cultura generale\n");
-  printf("+++++++++++++++++++++++++++++\n");
-  printf("La tua scelta: ");
+    clearScreen();
+    printf("Quiz disponibili\n");
+    printf("+++++++++++++++++++++++++++++\n");
+    printf("1 - Curiosita sulla tecnologia\n");
+    printf("2 - Cultura generale\n");
+    printf("+++++++++++++++++++++++++++++\n");
+    printf("La tua scelta: ");
 }
 
 void handleQuiz(int sock, std::string theme) {
-  char buffer[BUFFER_SIZE];
-  std::string userInput;
-  bool quizActive = true;
+    char buffer[BUFFER_SIZE];
+    std::string userInput;
+    bool quizActive = true;
 
-  while (quizActive) {
-    memset(buffer, 0, BUFFER_SIZE);
-    int bytesRead = read(sock, buffer, BUFFER_SIZE);
-    if (bytesRead <= 0) {
-      perror("Server disconnected or error reading from server");
-      break;
+    while (quizActive) {
+        memset(buffer, 0, BUFFER_SIZE);
+        int bytesRead = read(sock, buffer, BUFFER_SIZE);
+        if (bytesRead <= 0) {
+            perror("Server disconnected or error reading from server");
+            break;
+        }
+
+        clearScreen();
+        std::string qThemes = (theme == "1") ? "Curiosita sulla tecnologia" : "Cultura generale";
+        printf("Quiz - %s\n", qThemes.c_str());
+        printf("+++++++++++++++++++++++++++++\n");
+        printf("\n%s\n", buffer);
+
+        if (strstr(buffer, "Quiz completed!") ||
+            strstr(buffer, "Quiz ended") ||
+            strstr(buffer, "already completed")) {
+            printf("\nPress Enter to continue...\n");
+            std::getline(std::cin, userInput);
+            quizActive = false;
+            continue;
+        }
+
+        std::getline(std::cin, userInput);
+        send(sock, userInput.c_str(), userInput.size(), 0);
+
+        bool correct;
+        read(sock, &correct, sizeof(correct));
+
+        clearScreen();
+        if (correct) {
+            printf("\nCorrect answer!\n");
+        } else {
+            printf("\nWrong answer\n");
+        }
+        // printf("\nPress Enter to continue...\n");
+        std::getline(std::cin, userInput);
     }
-
-    clearScreen();
-    std::string qThemes = (theme == "1") ? "Curiosita sulla tecnologia" : "Cultura generale";
-    printf("Quiz - %s\n", qThemes.c_str());
-    printf("+++++++++++++++++++++++++++++\n");
-    printf("\n%s\n", buffer);
-
-    if (strstr(buffer, "Quiz completed!") ||
-        strstr(buffer, "Quiz ended") ||
-        strstr(buffer, "already completed")) {
-      printf("\nPress Enter to continue...\n");
-      std::getline(std::cin, userInput);
-      quizActive = false;
-      continue;
-    }
-
-    std::getline(std::cin, userInput);
-    send(sock, userInput.c_str(), userInput.size(), 0);
-  }
 }
 
 int main(int argc, char* argv[]) {
-  if (argc != 2) {
-    printf("Usage: %s <port>\n", argv[0]);
-    return 1;
-  }
-
-  int port = atoi(argv[1]);
-  int sock = 0;
-  struct sockaddr_in serverAddr;
-  std::string userInput;
-  std::string nickname;
-  std::string theme;
-  bool running = true;
-
-  while (running) {
-    sNickname();
-    std::getline(std::cin, nickname);
-    frontPage();
-    std::getline(std::cin, userInput);
-
-    if (userInput == "1") {
-      sock = socket(AF_INET, SOCK_STREAM, 0);
-      if (sock < 0) {
-        perror("Error creating socket");
+    if (argc != 2) {
+        printf("Usage: %s <port>\n", argv[0]);
         return 1;
-      }
-
-      serverAddr.sin_family = AF_INET;
-      serverAddr.sin_port = htons(port);
-
-      if (inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr) <= 0) {
-        perror("Invalid address");
-        close(sock);
-        continue;
-      }
-
-      if (connect(sock, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
-        perror("Connection failed");
-        close(sock);
-        continue;
-      }
-
-      send(sock, nickname.c_str(), nickname.size(), 0);
-
-      sTheme();
-      std::getline(std::cin, theme);
-      send(sock, theme.c_str(), theme.size(), 0);
-
-      handleQuiz(sock, theme);
-      close(sock);
-    } else if (userInput == "2") {
-      running = false;
-    } else {
-      printf("\nInvalid option. Press Enter to continue...\n");
-      std::getline(std::cin, userInput);
     }
-  }
 
-  return 0;
+    int port = atoi(argv[1]);
+    int sock = 0;
+    struct sockaddr_in serverAddr;
+    std::string userInput;
+    std::string nickname;
+    std::string theme;
+    bool running = true;
+
+    while (running) {
+        frontPage();
+        std::getline(std::cin, userInput);
+
+        if (userInput == "1") {
+            sock = socket(AF_INET, SOCK_STREAM, 0);
+            if (sock < 0) {
+                perror("Error creating socket");
+                return 1;
+            }
+
+            serverAddr.sin_family = AF_INET;
+            serverAddr.sin_port = htons(port);
+
+            if (inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr) <= 0) {
+                perror("Invalid address");
+                close(sock);
+                continue;
+            }
+
+            if (connect(sock, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
+                perror("Connection failed");
+                close(sock);
+                continue;
+            }
+
+            bool an = false;
+            while(!an) {
+              sNickname();
+              std::getline(std::cin, nickname);
+              send(sock, nickname.c_str(), nickname.size(), 0);
+              if(read(sock, &an, sizeof(an)) == false) {
+               printf("Nickname gia in uso, scegline un altro\n");
+              } else {
+                an = true;
+                clearScreen();
+                printf("Benvenuto %s!\n", nickname.c_str());
+                std::getline(std::cin, userInput);
+              }
+            }
+
+            sTheme();
+            std::getline(std::cin, theme);
+            // send(sock, theme.c_str(), theme.size(), 0);
+
+            handleQuiz(sock, theme);
+            close(sock);
+        } else if (userInput == "2") {
+            running = false;
+        } else {
+            printf("\nInvalid option. Press Enter to continue...\n");
+            std::getline(std::cin, userInput);
+        }
+    }
+
+    return 0;
 }
--- server.cpp
+++ server.cpp
@@ -23,17 +23,16 @@
 #define MAX_CLIENTS 100
 
 struct Player {
-  std::string nombre;
-  int techScore;
-  int generalScore;
-  int quizTheme;
-  bool hasCompletedTech;
-  bool hasCompletedGeneral;
+    std::string nombre;
+    int techScore;
+    int generalScore;
+    bool hasCompletedTech;
+    bool hasCompletedGeneral;
 };
 
 struct qQuestion {
-  std::string question;
-  std::string answer;
+    std::string question;
+    std::string answer;
 };
 
 std::vector<qQuestion> techQuestions;
@@ -46,21 +45,21 @@ void clearScreen() {
 }
 
 std::vector<qQuestion> loadQuestions(std::string& filename) {
-  std::vector<qQuestion> questions;
-  std::ifstream file(filename);
-  std::string line;
+    std::vector<qQuestion> questions;
+    std::ifstream file(filename);
+    std::string line;
 
-  while (std::getline(file, line)) {
-    size_t delimiterPos = line.find('|');
-    if (delimiterPos != std::string::npos) {
-      qQuestion question;
-      question.question = line.substr(0, delimiterPos);
-      question.answer = line.substr(delimiterPos + 1);
-      questions.push_back(question);
+    while (std::getline(file, line)) {
+        size_t delimiterPos = line.find('|');
+        if (delimiterPos != std::string::npos) {
+            qQuestion question;
+            question.question = line.substr(0, delimiterPos);
+            question.answer = line.substr(delimiterPos + 1);
+            questions.push_back(question);
+        }
     }
-  }
 
-  return questions;
+    return questions;
 }
 
 void moveCursor(int x, int y) {
@@ -68,13 +67,13 @@ void moveCursor(int x, int y) {
 }
 
 bool uniqueName(std::string name) {
-  std::lock_guard<std::mutex> lock(playersMutex);
-  for (const auto& player : players) {
-    if (player.second.nombre == name) {
-      return false;
+    std::lock_guard<std::mutex> lock(playersMutex);
+    for (const auto& player : players) {
+        if (player.second.nombre == name) {
+            return false;
+        }
     }
-  }
-  return true;
+    return true;
 }
 
 void printScoreboard() {
@@ -93,14 +92,14 @@ void printScoreboard() {
 
     printf("Puntaggio tema 1\n");
     for (const auto& player : players) {
-      if(player.second.techScore != 0)
-        printf("%s: %d\n", player.second.nombre.c_str(), player.second.techScore);
+        if(player.second.techScore != 0)
+            printf("%s: %d\n", player.second.nombre.c_str(), player.second.techScore);
     }
 
     printf("Puntaggio tema 2\n");
     for (const auto& player : players) {
-      if(player.second.generalScore != 0)
-        printf("%s: %d\n", player.second.nombre.c_str(), player.second.generalScore);
+        if(player.second.generalScore != 0)
+            printf("%s: %d\n", player.second.nombre.c_str(), player.second.generalScore);
     }
 
     printf("Quiz Tema 1 completato\n");
@@ -121,239 +120,230 @@ void printScoreboard() {
 }
 
 void updateScoreboard() {
-  while (true) {
-    printScoreboard();
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-  }
+    while (true) {
+        printScoreboard();
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
 }
 
 void handleNewPlayer(int socket) {
-  char buffer[BUFFER_SIZE];
-  std::string nickname;
-  bool validNickname = false;
+    char buffer[BUFFER_SIZE];
+    bool validNickname = false;
 
-  while (!validNickname) {
-    int bytes = read(socket, buffer, BUFFER_SIZE);
-    if (bytes <= 0) {
-      close(socket);
-      return;
+    while (!validNickname) {
+        ssize_t bytesRead = read(socket, buffer, BUFFER_SIZE);
+        if (bytesRead <= 0) {
+            close(socket);
+            return;
+        }
+
+        std::string nickname(buffer, bytesRead);
+        nickname.erase(std::remove(nickname.begin(), nickname.end(), '\n'), nickname.end());
+
+
+        if (uniqueName(nickname)) {
+            validNickname = true;
+            Player newPlayer;
+            newPlayer.nombre = nickname;
+            newPlayer.techScore = 0;
+            newPlayer.generalScore = 0;
+            newPlayer.hasCompletedTech = false;
+            newPlayer.hasCompletedGeneral = false;
+            newPlayer.quizTheme = 0;
+
+            std::lock_guard<std::mutex> lock(playersMutex);
+            players[socket] = newPlayer;
+
+            std::cerr << "Nickname accepted\n";
+            send(socket, &validNickname, sizeof(validNickname), 0);
+        } else {
+            std::cerr << "Nickname already in use\n";
+            send(socket, &validNickname, sizeof(validNickname), 0);
+        }
     }
-    buffer[bytes] = '\0';
-    nickname = std::string(buffer);
-    nickname.erase(std::remove(nickname.begin(), nickname.end(), '\n'), nickname.end());
-
-    if (uniqueName(nickname)) {
-      validNickname = true;
-      Player newPlayer;
-      newPlayer.nombre = nickname;
-      newPlayer.techScore = 0;
-      newPlayer.generalScore = 0;
-      newPlayer.hasCompletedTech = false;
-      newPlayer.hasCompletedGeneral = false;
-      newPlayer.quizTheme = 0;
-
-      std::lock_guard<std::mutex> lock(playersMutex);
-      players[socket] = newPlayer;
-
-      send(socket, "Nickname accepted. Choose a quiz theme (1 or 2):\n", 49, 0);
-    } else {
-      send(socket, "Nickname already taken. Try another one:\n", 40, 0);
-    }
-  }
 }
 
 void handleQuiz(int socket, int theme) {
-  Player& player = players[socket];
-  std::vector<qQuestion>& questions = (theme == 1) ? techQuestions : generalQuestions;
-  int& score = (theme == 1) ? player.techScore : player.generalScore;
-  bool& completed = (theme == 1) ? player.hasCompletedTech : player.hasCompletedGeneral;
+    Player& player = players[socket];
+    std::vector<qQuestion>& questions = (theme == 1) ? techQuestions : generalQuestions;
+    int& score = (theme == 1) ? player.techScore : player.generalScore;
+    bool& completed = (theme == 1) ? player.hasCompletedTech : player.hasCompletedGeneral;
 
-  if (completed) {
-    send(socket, "You have already completed this quiz.\n", 36, 0);
-    return;
-  }
-
-  player.quizTheme = theme;
-  char buffer[BUFFER_SIZE];
-
-  for (int i = 0; i < questions.size(); i++) {
-    std::string questionMsg = "Question " + std::to_string(i + 1) + ": " + questions[i].question + "\n";
-    send(socket, questionMsg.c_str(), questionMsg.length(), 0);
-
-    int bytes = read(socket, buffer, BUFFER_SIZE);
-    if (bytes <= 0) {
-      break;
-    }
-    buffer[bytes] = '\0';
-    std::string answer(buffer);
-    answer.erase(std::remove(answer.begin(), answer.end(), '\n'), answer.end());
-
-    if (answer == "show score") {
-      std::string scoreBoard = "Current Scores:\nTechnology Quiz:\n";
-      for (const auto& p : players) {
-        scoreBoard += p.second.nombre + ": " + std::to_string(p.second.techScore) + "\n";
-      }
-      scoreBoard += "\nGeneral Knowledge Quiz:\n";
-      for (const auto& p : players) {
-        scoreBoard += p.second.nombre + ": " + std::to_string(p.second.generalScore) + "\n";
-      }
-      send(socket, scoreBoard.c_str(), scoreBoard.length(), 0);
-      i--;
-      continue;
+    if (completed) {
+        send(socket, "You have already completed this quiz.\n", 36, 0);
+        return;
     }
 
-    if (answer == "endquiz") {
-      players.erase(socket);
-      send(socket, "Quiz ended. Goodbye!\n", 20, 0);
-      return;
+    player.quizTheme = theme;
+    char buffer[BUFFER_SIZE];
+
+    for (int i = 0; i < questions.size(); i++) {
+        std::string questionMsg = "Question " + std::to_string(i + 1) + ": " + questions[i].question + "\n";
+        send(socket, questionMsg.c_str(), questionMsg.length(), 0);
+
+        int bytes = read(socket, buffer, BUFFER_SIZE);
+        if (bytes <= 0) {
+            break;
+        }
+        buffer[bytes] = '\0';
+        std::string answer(buffer);
+        answer.erase(std::remove(answer.begin(), answer.end(), '\n'), answer.end());
+
+        bool correct = (answer == questions[i].answer);
+        send(socket, &correct, sizeof(correct), 0);
+
+        if (correct) {
+            score++;
+        }
     }
 
-    if (answer == questions[i].answer) {
-      score++;
-      send(socket, "Correct answer!\n", 15, 0);
-    } else {
-      send(socket, "Wrong answer!\n", 13, 0);
-    }
-  }
-
-  completed = true;
-  std::string finalMsg = "Quiz completed! Your final score: " + std::to_string(score) + "\n";
-  send(socket, finalMsg.c_str(), finalMsg.length(), 0);
+    completed = true;
+    std::string finalMsg = "Quiz completed! Your final score: " + std::to_string(score) + "\n";
+    send(socket, finalMsg.c_str(), finalMsg.length(), 0);
 }
 
-void run(int sd) {
-  char buffer[BUFFER_SIZE];
-  int bytes = read(sd, buffer, BUFFER_SIZE);
-  buffer[bytes] = '\0';
-  std::string command(buffer);
-  command.erase(std::remove(command.begin(), command.end(), '\n'), command.end());
+// Function to handle received commands
+void run(int clientSocket) {
+    // char buffer[BUFFER_SIZE];
+    // int bytes = read(clientSocket, buffer, BUFFER_SIZE);
+    // buffer[bytes] = '\0';
+    // std::string command(buffer);
+    // command.erase(std::remove(command.begin(), command.end(), '\n'), command.end());
 
-  auto it = players.find(sd);
-  if (it == players.end()) {
-    handleNewPlayer(sd);
-  } else {
-    if (it->second.quizTheme == 0) {
-    try {
-        int theme = std::stoi(command);
-        if (theme == 1 || theme == 2) {
-        handleQuiz(sd, theme);
-        } else {
-        std::string errorMsg = "Invalid theme. Please choose theme 1 or 2\n";
-        send(sd, errorMsg.c_str(), errorMsg.length(), 0);
+    auto it = players.find(clientSocket);
+    if (it == players.end()) {
+        handleNewPlayer(clientSocket);
+        std::cerr << "New player connected\n";
+    } else {
+        if (it->second.quizTheme == 0) {
+            try {
+                char buffer[BUFFER_SIZE];
+                ssize_t bytesRead = read(clientSocket, buffer, BUFFER_SIZE);
+                if (bytesRead > 0) {
+                    std::string theme(buffer, bytesRead);
+                    theme.erase(std::remove(theme.begin(), theme.end(), '\n'), theme.end());
+                    int themeNum = std::stoi(theme);
+                    if (themeNum == 1 || themeNum == 2) {
+                        handleQuiz(clientSocket, themeNum);
+                    } else {
+                        std::string errorMsg = "Invalid theme. Please choose theme 1 or 2\n";
+                        send(clientSocket, errorMsg.c_str(), errorMsg.length(), 0);
+                    }
+                }
+            } catch (const std::invalid_argument& e) {
+                std::string errorMsg = "Invalid input. Please enter a number (1 or 2)\n";
+                send(clientSocket, errorMsg.c_str(), errorMsg.length(), 0);
+            } catch (const std::out_of_range& e) {
+                std::string errorMsg = "Invalid theme. Please choose theme 1 or 2\n";
+                send(clientSocket, errorMsg.c_str(), errorMsg.length(), 0);
+            }
         }
-    } catch (const std::invalid_argument& e) {
-        std::string errorMsg = "Invalid input. Please enter a number (1 or 2)\n";
-        send(sd, errorMsg.c_str(), errorMsg.length(), 0);
-    } catch (const std::out_of_range& e) {
-        std::string errorMsg = "Invalid theme. Please choose theme 1 or 2\n";
-        send(sd, errorMsg.c_str(), errorMsg.length(), 0);
     }
-    }
-  }
 }
 
 int main(int argc, char* argv[]) {
-  std::thread t(updateScoreboard);
-  t.detach();
+    std::thread t(updateScoreboard);
+    t.detach();
 
-  std::string generalFile = "general.txt";
-  std::string techFile = "tech.txt";
+    std::string generalFile = "general.txt";
+    std::string techFile = "tech.txt";
 
-  try {
-    generalQuestions = loadQuestions(generalFile);
-  } catch (std::exception& e) {
-    std::cerr << e.what() << std::endl;
-  }
-
-  try {
-    techQuestions = loadQuestions(techFile);
-  } catch (std::exception& e) {
-    std::cerr << e.what() << std::endl;
-  }
-
-  int serverSocket, maxSd, sd, activity, newSocket;
-  struct sockaddr_in serverAddr, clientAddr;
-  int playerSockets[MAX_CLIENTS] = {0};
-  socklen_t addrLen;
-  fd_set readfds;
-  char buffer[BUFFER_SIZE];
-
-  serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-  if (serverSocket == -1) {
-    perror("Error creating socket");
-    exit(EXIT_FAILURE);
-  }
-
-  serverAddr.sin_family = AF_INET;
-  serverAddr.sin_addr.s_addr = INADDR_ANY;
-  serverAddr.sin_port = htons(PORT);
-
-  if (bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
-    perror("Error binding");
-    close(serverSocket);
-    exit(EXIT_FAILURE);
-  }
-
-  if (listen(serverSocket, 3) < 0) {
-    perror("Error listening");
-    close(serverSocket);
-    exit(EXIT_FAILURE);
-  }
-
-  addrLen = sizeof(clientAddr);
-
-  while (true) {
-    FD_ZERO(&readfds);
-    FD_SET(serverSocket, &readfds);
-    maxSd = serverSocket;
-
-    for (int i = 0; i < MAX_CLIENTS; i++) {
-      sd = playerSockets[i];
-      if (sd > 0) {
-        FD_SET(sd, &readfds);
-      }
-      if (sd > maxSd) {
-        maxSd = sd;
-      }
+    try {
+        generalQuestions = loadQuestions(generalFile);
+    } catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
     }
 
-    activity = select(maxSd + 1, &readfds, NULL, NULL, NULL);
-    if ((activity < 0) && (errno != EINTR)) {
-      perror("Error select");
+    try {
+        techQuestions = loadQuestions(techFile);
+    } catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
     }
 
-    if (FD_ISSET(serverSocket, &readfds)) {
-      if ((newSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &addrLen)) < 0) {
-        perror("Error accept");
+    int serverSocket,playerSocket, maxSd, sd, activity, newSocket;
+    struct sockaddr_in serverAddr, clientAddr;
+    int playerSockets[MAX_CLIENTS] = {0};
+    socklen_t addrLen;
+    fd_set readfds;
+    char buffer[BUFFER_SIZE];
+
+    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (serverSocket < 0) {
+        perror("Error creating socket");
         exit(EXIT_FAILURE);
-      }
-
-      handleNewPlayer(newSocket);
-
-      for (int i = 0; i < MAX_CLIENTS; i++) {
-        if (playerSockets[i] == 0) {
-          playerSockets[i] = newSocket;
-          break;
-        }
-      }
     }
 
-    for (int i = 0; i < MAX_CLIENTS; i++) {
-      sd = playerSockets[i];
-      if (FD_ISSET(sd, &readfds)) {
-        int valRead = read(sd, buffer, BUFFER_SIZE);
-        if (valRead == 0) {
-          getpeername(sd, (struct sockaddr*)&clientAddr, &addrLen);
-          printf("Client disconnected, ip %s, port %d\n", inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port));
-          close(sd);
-          playerSockets[i] = 0;
-        } else {
-          run(sd);
-        }
-      }
-    }
-  }
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_port = htons(PORT);
 
-  close(serverSocket);
-  return 0;
+    if (bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
+        perror("Error binding");
+        close(serverSocket);
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(serverSocket, 3) < 0) {
+        perror("Error listening");
+        close(serverSocket);
+        exit(EXIT_FAILURE);
+    }
+
+    addrLen = sizeof(clientAddr);
+
+    while (true) {
+        FD_ZERO(&readfds);
+        FD_SET(serverSocket, &readfds);
+        maxSd = serverSocket;
+
+        for (int i = 0; i < MAX_CLIENTS; i++) {
+            sd = playerSockets[i];
+            if (sd > 0) {
+                FD_SET(sd, &readfds);
+            }
+            if (sd > maxSd) {
+                maxSd = sd;
+            }
+        }
+
+        activity = select(maxSd + 1, &readfds, NULL, NULL, NULL);
+        if ((activity < 0) && (errno != EINTR)) {
+            perror("Error select");
+        }
+
+        if (FD_ISSET(serverSocket, &readfds)) {
+            if ((newSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &addrLen)) < 0) {
+                perror("Error accept");
+                exit(EXIT_FAILURE);
+            }
+
+            // handleNewPlayer(newSocket);
+
+            for (int i = 0; i < MAX_CLIENTS; i++) {
+                if (playerSockets[i] == 0) {
+                    playerSockets[i] = newSocket;
+                    break;
+                }
+            }
+        }
+
+        for (int i = 0; i < MAX_CLIENTS; i++) {
+            sd = playerSockets[i];
+            if (FD_ISSET(sd, &readfds)) {
+                int valRead = read(sd, buffer, BUFFER_SIZE);
+                if (valRead == 0) {
+                    getpeername(sd, (struct sockaddr*)&clientAddr, &addrLen);
+                    printf("Client disconnected, ip %s, port %d\n", inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port));
+                    close(sd);
+                    playerSockets[i] = 0;
+                } else {
+                    run(sd);
+                }
+            }
+        }
+    }
+
+    close(playerSocket);
+    return 0;
 }
